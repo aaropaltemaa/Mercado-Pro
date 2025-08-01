80114601bd1ac74b1f77178a0a1ae52ea982bdbe
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-01 15:57:39 +0300

    added tooltip & disabled elements on placeorder button

--- client/src/components/PlaceOrderButton.tsx
+++ client/src/components/PlaceOrderButton.tsx
@@ -3,10 +3,12 @@ import orderService from "../services/orders";
 import { useAuthStore } from "../store/auth";
 import { useCart } from "../store/cart";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "react-tooltip";
 
 const PlaceOrderButton = () => {
   const token = useAuthStore((state) => state.token);
   const clearCart = useCart((state) => state.clearCart);
+  const cartItems = useCart((state) => state.cartItems);
   const navigate = useNavigate();
 
   const placeOrder = async (token: string) => {
@@ -23,15 +25,23 @@ const PlaceOrderButton = () => {
   return (
     <div>
       <button
-        className="font-bold border rounded-xl p-3 text-white transition hover:bg-green-500 bg-blue-600"
+        data-tooltip-id="order-tip"
+        className="font-bold border rounded-xl p-3 text-white transition 
+            hover:bg-green-500 bg-blue-600 
+            disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled={!token || cartItems.length === 0}
         onClick={() => {
-          if (token) {
+          if (token && cartItems.length !== 0) {
             placeOrder(token);
           }
         }}
       >
         Place order
       </button>
+      <Tooltip
+        id="order-tip"
+        content="Add items to your cart to place an order."
+      />
     </div>
   );
 };
