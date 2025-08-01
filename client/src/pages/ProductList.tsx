6fc433c8d11dcab75b805b58261a869a841a0300
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-01 16:29:44 +0300

    transformed product elements into corresponding page links

--- client/src/pages/ProductList.tsx
+++ client/src/pages/ProductList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import productService from "../services/products";
+import { Link } from "react-router-dom";
 
 type Product = {
   id: string;
@@ -22,11 +23,15 @@ export default function ProductList() {
       <h1 className="text-2xl font-bold mb-4">Products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {products.map((product) => (
-          <div key={product.id} className="border p-4 rounded shadow">
+          <Link
+            to={`products/${product.id}`}
+            key={product.id}
+            className="border p-4 rounded shadow transition hover:opacity-60"
+          >
             <h2 className="font-semibold text-lg">{product.name}</h2>
             <p className="text-sm text-gray-600">{product.description}</p>
             <p className="font-bold mt-2">${product.price}</p>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
