9a7fcc867eaeed1a097283d0201a6060218f9b0d
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-01 20:18:28 +0300

    added method for fetching a single product from backend

--- client/src/services/products.ts
+++ client/src/services/products.ts
@@ -1,14 +1,23 @@
 import axios from "axios";
 
-const url = "http://localhost:3000/products"
+const url = "http://localhost:3000/products";
 
 const getAll = () => {
   const req = axios.get(url);
   return req.then((res) => res.data);
 };
 
-const create = (productData: { name: string, description: string, price: number }) => {  
-    const req = axios.post(url, productData)
-    return req.then((res) => res.data)
-}
-export default { getAll, create }
+const getOne = (id: string) => {
+  const req = axios.get(`${url}/${id}`);
+  return req.then((res) => res.data);
+};
+
+const create = (productData: {
+  name: string;
+  description: string;
+  price: number;
+}) => {
+  const req = axios.post(url, productData);
+  return req.then((res) => res.data);
+};
+export default { getAll, create, getOne };
