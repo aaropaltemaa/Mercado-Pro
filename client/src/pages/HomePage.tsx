50590bb608db7872a8004bf42a1132ac89a3be73
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-02 20:34:56 +0300

    created initial design for hero section

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
+import HeroSection from "../components/HeroSection";
 import ProductList from "../components/ProductList";
 
 const HomePage = () => {
   return (
-    <div>
-      <ProductList />
+    <div className="w-full">
+      <div className="w-full px-4 mb-12">
+        <HeroSection />
+      </div>
+      <div className="max-w-7xl mx-auto px-4">
+        <ProductList />
+      </div>
     </div>
   );
 };
