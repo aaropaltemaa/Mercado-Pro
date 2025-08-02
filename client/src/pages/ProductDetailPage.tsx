ec8779370530103bebe3b5eedffd6105cab31751
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-02 11:04:19 +0300

    implemented a addtocart handler with toast feedback for handling post cart requests

--- client/src/pages/ProductDetailPage.tsx
+++ client/src/pages/ProductDetailPage.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import productService from "../services/products";
+import cartService from "../services/cart";
 import type { Product } from "../../../types";
 import { FaCartPlus } from "react-icons/fa";
+import { useAuthStore } from "../store/auth";
+import toast from "react-hot-toast";
 
 const ProductDetailPage = () => {
   const productId = useParams().id;
+  const token = useAuthStore((state) => state.token);
   const [product, setProduct] = useState<Product | null>(null);
 
   useEffect(() => {
@@ -14,8 +18,13 @@ const ProductDetailPage = () => {
     }
   }, [productId]);
 
-  const addToCart = () => {
-    console.log("click");
+  const addToCart = async (id: string) => {
+    try {
+      await cartService.addCartItem(id, 1, token ?? "");
+      toast.success("Succesfully added item to cart!");
+    } catch {
+      toast.error("Failed to add item to cart");
+    }
   };
 
   return product ? (
@@ -24,7 +33,7 @@ const ProductDetailPage = () => {
       <div>${product.price}</div>
       <div>{product.description}</div>
       <button
-        onClick={addToCart}
+        onClick={() => addToCart(product.id)}
         className="flex items-center gap-2 px-3 py-3 max-w-36 rounded-xl bg-green-600 text-white text-sm hover:bg-green-700 transition"
       >
         <FaCartPlus size={18} />
