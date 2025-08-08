577b9bd1e2943b9e5662ce447e96cf620a35df95
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-08 13:24:36 +0300

    added category enum in prisma schema for filtering products + new prisma migration

---
+++ server/prisma/migrations/20250808102312_add_category_productfield/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "image" TEXT NOT NULL,
+    "sellerId" TEXT,
+    "category" TEXT NOT NULL DEFAULT 'Other',
+    CONSTRAINT "Product_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("createdAt", "description", "id", "image", "name", "price", "sellerId") SELECT "createdAt", "description", "id", "image", "name", "price", "sellerId" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
