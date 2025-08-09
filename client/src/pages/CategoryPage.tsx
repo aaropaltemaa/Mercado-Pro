8a8dbe017ee9a69ffce445392fb5f501c8a46bd0
Aaro Paltemaa <apaltemaa@gmail.com>
2025-08-09 15:55:34 +0300

    created headers for each category page

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+
 const CategoryPage = () => {
-  return <div>categorypage</div>;
+  const { categoryName } = useParams<{ categoryName: string }>();
+  const capitalizedCategoryName = categoryName
+    ? categoryName.charAt(0).toUpperCase() + categoryName.slice(1)
+    : "";
+
+  return (
+    <div>
+      <h2 className="text-2xl sm:text-3xl font-bold mb-8">
+        {capitalizedCategoryName}
+      </h2>
+    </div>
+  );
 };
 
 export default CategoryPage;
